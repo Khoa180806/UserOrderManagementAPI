ffe21eb1027cb287194e0daba14d70adb9545331
Khoa180806 <khoaphan180806@gmail.com>
2026-05-22 15:13:21 +0700

    Create base entity ABP Style

---
+++ UserOrderManagerment/Entities/AuditedEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace UserOrderManagerment.Entities;
+
+public abstract class AuditedEntity
+{
+    public int Id { get; set; }
+
+    public DateTime CreationTime { get; set; }
+    public int? CreatorUserId { get; set; }
+
+    public DateTime? LastModificationTime { get; set; }
+    public bool IsDeleted { get; set; }
+}
